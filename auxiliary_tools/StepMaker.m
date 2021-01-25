44409820faa6a0278241687ff2e10baf280f905d
luukloeff <l.loeff@gmail.com>
2021-01-25 12:57:20 +0100

    Add files via upload

--- auxiliary_tools/StepMaker.m
+++ auxiliary_tools/StepMaker.m
@@ -48,222 +48,10 @@ initval.addbase         =str2double(get(handles.addbase,'String'));
 initval.repeatsteps     =str2double(get(handles.traces,'String'));
 stepmaker_mainloop(initval)
  
-function stepmaker_mainloop(initval)
-%JWJK_B:-------------------------------------------------------------------
-%Title:Simulate Step Traces
-%Summary: %this function  creates a simple stepped trace with noise. 
-%Approach: Steps  are taken as random samples from a 
-%stepsize - and dwell time distribution. 
-%This distribution can be refined in the 'Set_type_of_signal' function
+     function stepmaker_mainloop(initval)
+         %%insertcode here!
+         initval
 
-%Dwell times can be set as 'dependent' on the step size or 'independent'.
-%In the latter case they are taken as random samples from a flat or
-%exponential dwell time distribution.
-
-%By varying the settings, a wide range of traces types can be generated
-
-%Below, example settings are given for case studies
-
-%Input: none
-%Output: step trace saved as text column in root path
-
-%Jacob Kerssemakers, Cees Dekker Lab, 2018-20
-%JWJK_B:-------------------------------------------------------------------
-
-% if nargin<1
-%     %definition of steps per type
-%     initval.flatstep=1;       %type flat
-%     initval.minstep=8;         
-%     initval.maxstep=8;         
-%     %---------------------------------
-%     initval.gausstep=0;       %type gauss
-%     initval.meanstep=10;      
-%     initval.sigmastep=20;       
-%     %---------------------------------
-%     initval.expstep=0;         %type exponential
-%     initval.decaystep=50;      
-%     %---------------------------------
-%     
-%     %definition of dwells per type
-%     initval.flatdwell=1;      %type Flat 
-%     initval.mindwell=1; 
-%     initval.maxdwell=100;       
-%     %---------------------------------
-%     initval.gausdwell=0;      %type Gauss 
-%     initval.meandwell=100;      
-%     initval.sigmadwell=100;      
-%     %----------------------------------
-%     initval.expdwell=0;      %type exponential
-%     initval.decaydwell=100;      
-%     %---------------------------------
-% 
-%     %trace properties
-%     initval.stepsnumber=6;     %number of steps
-%     initval.noisesteps=1;      %noise level   
-%     initval.addbase=-700;        %add a L-points baseline before (-L) or after (+L)
-%     initval.repeatsteps=3;    %repeat the number of 'blocks' per trace
-%     initval.repeats=5;        %repeat the generation of traces
-% end
- 
-%% distribution generation
-
-        %Build dwell distribution    
-        [dwell_distribution,binaxis_dwells,initval]=Set_dwell_distributions(initval,50)          
-        summed_dwell_distribution=cumsum(dwell_distribution);
-        
-        %%Build step distribution
-        [step_distribution,binaxis_steps,initval]=Set_step_distributions(initval,50);         
-        summed_stepdistribution=cumsum(step_distribution); 
-
-%% Build lists picking a random sample from the step and dwell distributions        
-for rp=1:initval.repeats
-    Concat_Curve=[];
-    for bl=1:initval.repeatsteps 
-        % Build step size list  
-        StepSizes=zeros(initval.stepsnumber,1);  
-        for jj=1:initval.stepsnumber+1  
-                step_sample=Pick_from_distribution(binaxis_steps,summed_stepdistribution);
-                StepSizes(jj)=step_sample;
-        end 
-       % Build dwell-time list 
-       Dwelltimes=zeros(initval.stepsnumber,1); 
-        for jj=1:initval.stepsnumber+1
-            dwell_sample=Pick_from_distribution(binaxis_dwells,summed_dwell_distribution);
-            DwellTimes(jj)=round(dwell_sample);
-        end
- 
-
-        %% Build curve     
-        Curve=[];
-        Level=0;
-        for jj=1:initval.stepsnumber+1
-            Dwell=DwellTimes(jj);
-            Step=StepSizes(jj);        
-            NewSection=Level+Step*ones(Dwell,1);        
-            Curve=[Curve ; NewSection];
-            Level=Curve(end);
-        end  
-
-        % Add start or tail
-        if initval.addbase<0,Curve=[zeros(abs(initval.addbase),1)+Curve(1) ; Curve];end
-        if initval.addbase>0,Curve=[Curve ; zeros(abs(initval.addbase),1)+Curve(end)];end       
-        
-        Concat_Curve=[Concat_Curve; Curve];
-     end
-        
-        
-        
-    %% Finalize:
-    %Add time axis, noise
-    Ld=length(Concat_Curve);
-    TimAx=(1:Ld)';
-    Noise=initval.noisesteps*randn(Ld,1);
-    Trace=Concat_Curve+Noise; 
-    data=[TimAx Trace];
-    
-
-    
-    
-    
-    %% plot
-    figure(169);
-    subplot(2,2,1);
-        bar(binaxis_steps,step_distribution, 'w');
-        title('Step Sizes');
-        xlabel('step size, a.u.');
-        ylabel('occurence, a.u.');
-        axis tight;
-    subplot(2,2,2); 
-        bar(binaxis_dwells,dwell_distribution, 'w');
-        title('Dwell Times');
-        xlabel('dwell time, pts');
-        ylabel('occurence, a.u.');
-        axis tight;
-    subplot(2,1,2);
-        plot(data(:,1),data(:,2), 'k-','Linewidth',2);
-        
-        title('Trace')
-    pause(0.1);
-
-
-
-    %% saveu
-    save_it=1;   
-    writepath=[pwd, '\test_traces'];
-    if ~isdir(writepath), mkdir(writepath);,end
-    SaveName=strcat('testdata',...
-        '_step_distribution',initval.TypeOfSteps,...
-        '_dwell_distribution',initval.TypeOfSteps,...
-        '_steps',num2str(initval.stepsnumber),...
-        '_blocks',num2str(bl),...
-        '_repeat',num2str(bl),num2str(rp),...
-        '.txt');
-    if save_it  
-        dlmwrite(strcat(writepath,'/',SaveName), data);
-        %saveas(gcf,strcat(writepath,'/',SaveName(1:end-4),'.jpg'));
-    end
-end
-    
- function Sample=Pick_from_distribution(BinAxis,SumCurve)
-            %pick from distribution
-            pickval=rand(1);  %pick a random percentile
-            sel_st=find(SumCurve>0);
-            NonZeroSumCurve=SumCurve(sel_st);
-            NonZeroBinAxis=BinAxis(sel_st);                        
-            %nearest occupied bin            
-            [~,idx]=min(abs(NonZeroSumCurve-pickval));
-            %will return first index of plateau which is correct
-            Sample=NonZeroBinAxis(idx);  
-                
-  
-   function [StepSizeCurve,binaxis_steps,initval]=Set_step_distributions(initval,binz)
-    %set up global range of stepsizes
-    if initval.flatstep==1,initval.TypeOfSteps='Flat';end
-    if initval.gausstep==1,initval.TypeOfSteps='Gaussian';end
-    if initval.expstep==1,initval.TypeOfSteps='Exponential';end  
-    switch initval.TypeOfSteps
-       case 'Flat'
-           %each step in the specified range has an equal chance of
-           %occurring
-           binaxis_steps=unique(linspace(initval.minstep,initval.maxstep,binz));
-           StepSizeCurve=(1+0*(binaxis_steps));         
-        case 'Gaussian'
-            minstep=initval.meanstep-2*initval.sigmastep;
-            maxstep=initval.meanstep+2*initval.sigmastep;
-            binaxis_steps=unique(linspace(minstep,maxstep,binz));
-            StepSizeCurve=exp(-( (binaxis_steps-initval.meanstep)/(initval.sigmastep)).^2);
-        case 'Exponential'  
-            binaxis_steps=unique(linspace(0,2*initval.decaystep,binz));
-            StepSizeCurve=exp(-binaxis_steps/(initval.decaystep)); 
-    end
-    StepSizeCurve=StepSizeCurve/sum(StepSizeCurve);  %normalized dstribution
-    
-    function [DwellTimeCurve,binaxis_dwells,initval]=Set_dwell_distributions(initval,binz)
-    %set up global range of stepsizes
-    if initval.flatdwell==1,initval.TypeOfDwells='Flat';end
-    if initval.gausdwell==1,initval.TypeOfDwells='Gaussian';end
-    if initval.expdwell==1, initval.TypeOfDwells='Exponential';end
-  
-    switch initval.TypeOfDwells
-       case 'Flat'
-           %each step in the specified range has an equal chance of
-           %occurring
-           mindwell=max([1 initval.mindwell]);
-           maxdwell=max([mindwell initval.maxdwell]);
-           binaxis_dwells=unique(linspace(mindwell,maxdwell,binz));
-           DwellTimeCurve=(1+0*(binaxis_dwells));         
-        case 'Gaussian'  %should be>0
-            mindwell=max([1 initval.meandwell-2*initval.sigmadwell]);;
-            maxdwell=max([mindwell initval.meandwell+2*initval.sigmadwell]);
-            binaxis_dwells=unique(linspace(mindwell,maxdwell,binz));
-            DwellTimeCurve=exp(-( (binaxis_dwells-initval.meandwell)/(initval.sigmadwell)).^2);
-        case 'Exponential'  
-            decay=max([1 initval.decaydwell]);
-            binaxis_dwells=unique(linspace(1,2*decay,binz));
-            DwellTimeCurve=exp(-binaxis_dwells/(decay)); 
-    end
-    DwellTimeCurve=DwellTimeCurve/sum(DwellTimeCurve);  %normalized dstribution
-    
     
 % --- Executes just before StepMaker is made visible.
 function StepMaker_OpeningFcn(hObject, eventdata, handles, varargin)
@@ -354,11 +142,6 @@ checkmax_mindwell=isnan(str2double(checkmax_mindwell));
          set(hObject,'String',50);
      return;
      end
-          if checkmax_mindwell < 1
-         msgbox('The min dwell time is smaller than 1. The input value has been set to 1','ERROR', 'error')
-         set(hObject,'String',1);
-     return;     
-end 
    
 
 
@@ -371,9 +154,9 @@ checkmax_meandwell=isnan(str2double(checkmax_meandwell));
          set(hObject,'String',75);
      return;
      end
-if checkmax_meandwell < 1
-         msgbox('The dwell time is smaller than 1. The input value has been set to 1','ERROR', 'error')
-         set(hObject,'String',1);
+if checkmax_meandwell < 2
+         msgbox('The number of steps is smaller than 1. The input value has been set to 1','ERROR', 'error')
+         set(hObject,'String',2);
      return;     
 end 
 
@@ -457,7 +240,8 @@ checkmax_repeats=isnan(str2double(checkmax_repeats));
          set(hObject,'String',1);
      return;
      end
-     if checkmax_repeats < 1
+checkmax_repeats1=str2double(get(hObject,'String'));     
+     if checkmax_repeats1 < 1
          msgbox('The number of repeats is smaller than 1. The input value has been set to 1','ERROR', 'error')
          set(hObject,'String',1);
      return;     
@@ -467,13 +251,14 @@ end
 
 function traces_Callback(hObject, ~, ~)
 checkmax_traces=get(hObject,'String');
-checkmax_traces=isnan(str2double(checkmax_traces));
-     if checkmax_traces==1
+checkmax_traces2=isnan(str2double(checkmax_traces));
+     if checkmax_traces2==1
          msgbox('The number of traces is NaN.','ERROR', 'error')
          set(hObject,'String',1);
      return;
      end
-if checkmax_traces < 1
+checkmax_traces1=str2double(get(hObject,'String'));
+if checkmax_traces1 < 1
          msgbox('The number of traces is smaller than 1. The input value has been set to 1','ERROR', 'error')
          set(hObject,'String',1);
      return;     
